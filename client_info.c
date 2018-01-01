ea3688a29ec9be85c3a662e12171806c60ffb176
TaiYi <931305033@qq.com>
2018-01-01 14:12:45 +0800

    test protocol

--- client_info.c
+++ client_info.c
@@ -2,40 +2,131 @@
 
 #include "cJson/cJSON.h"
 #include <stdio.h>
+#include <string.h>
 
-typedef struct {
-    char *IP;
-    int TPYE;
-    char *account;
-    char *passwd;
-    char *t;
-}LOGIN_INFO;
-
-void get_client_info(char *json) {
+void send_client_login_info(LOGIN_INFO *login_info,char *json) {
     cJSON *item;
-    LOGIN_INFO login_info;
     cJSON *root= cJSON_Parse(json);
 
     item=cJSON_GetObjectItem(root,"IP");
-    login_info.IP = item->valuestring;
+    login_info->IP = item->valuestring;
 
-    item = cJSON_GetObjectItem(root,"TYPE");
-    login_info.TPYE = item->valueint;
+    item = cJSON_GetObjectItem(root,"ACTION");
+    login_info->ACTION = item->valueint;
 
     item = cJSON_GetObjectItem(root,"account");
-    login_info.account = item->valuestring;
+    login_info->account = item->valuestring;
 
-    item = cJSON_GetObjectItem(root,"passwd");
-    login_info.passwd = item->valuestring;
+    item = cJSON_GetObjectItem(root,"password");
+    login_info->passwd = item->valuestring;
 
     item = cJSON_GetObjectItem(root,"time");
-    login_info.t = item->valuestring;
+    login_info->t = item->valuestring;
+}
 
-    printf("%s\n",login_info.IP);
-    printf("%s\n",login_info.account);
-    printf("%s\n",login_info.passwd);
-    printf("%s\n",login_info.t);
+void send_client_logout_info(LOGOUT_INFO *logout_info,char *json) {
+    cJSON *item;
+    cJSON *root= cJSON_Parse(json);
+
+    item=cJSON_GetObjectItem(root,"IP");
+    logout_info->IP = item->valuestring;
+
+    item = cJSON_GetObjectItem(root,"ACTION");
+    logout_info->ACTION = item->valueint;
+
+    item = cJSON_GetObjectItem(root,"account");
+    logout_info->account = item->valuestring;
+
+    item = cJSON_GetObjectItem(root,"time");
+    logout_info->t = item->valuestring;
+}
+
+void send_client_chat_info(CHAT_INFO *chat_info,char *json) {
+    cJSON *item;
+    cJSON *root= cJSON_Parse(json);
+
+    item=cJSON_GetObjectItem(root,"IP");
+    chat_info->IP = item->valuestring;
+
+    item = cJSON_GetObjectItem(root,"ACTION");
+    chat_info->ACTION = item->valueint;
+
+    item = cJSON_GetObjectItem(root,"account");
+    chat_info->account = item->valuestring;
+
+    item = cJSON_GetObjectItem(root,"time");
+    chat_info->t = item->valuestring;
+}
+
+void send_add_chat_info(ADD_INFO *chat_info,char *json) {
+    cJSON *item;
+    cJSON *root= cJSON_Parse(json);
+
+    item = cJSON_GetObjectItem(root,"ACTION");
+    chat_info->ACTION = item->valueint;
+
+    item = cJSON_GetObjectItem(root,"account");
+    chat_info->account = item->valuestring;
+}
 
 
-    cJSON_Delete(root);
+
+
+
+
+
+//反悔好友信息
+char* re_friend_info(FRIEND *aFriend , int len){
+    cJSON * cRoot = NULL;
+    cJSON * cFriend = NULL;
+    cJSON * cFriArray = NULL;
+
+    if((cRoot = cJSON_CreateObject()) == NULL || (cFriend = cJSON_CreateObject()) == NULL || (cFriArray = cJSON_CreateArray()) == NULL)
+    {
+        return NULL;
+    }
+    //好友列表
+    for (int i = 0; i < len; ++i) {
+        cJSON_AddStringToObject(cFriend, "u_name", aFriend->u_name);
+        cJSON_AddStringToObject(cFriend, "IP", aFriend->IP);
+        cJSON_AddItemToObject(cFriArray, "FRIEND", cFriend);
+        cFriend = cJSON_CreateObject();
+        aFriend++;
+    }
+
+    //数组添加到最外层JsonObject
+    cJSON_AddItemToObject(cRoot, "FRIEND", cFriArray);
+
+    char * pJson = cJSON_Print(cRoot);
+    if(NULL == pJson)
+    {
+        cJSON_Delete(cRoot);
+        cJSON_Delete(cFriend);
+        return NULL;
+    }
+
+    return pJson;
+}
+//反悔状态信息 7成功，0失败
+char* re_status_info(RE_INFO *re_info){
+    cJSON * cRoot = NULL;
+
+    if((cRoot = cJSON_CreateObject()) == NULL)
+    {
+        return NULL;
+    }
+
+    //添加状态码 成功：7 失败：0
+    cJSON_AddNumberToObject(cRoot, "STATUS",re_info->STATUS);
+    //时间戳
+    cJSON_AddStringToObject(cRoot, "TIME", re_info->t);
+
+    char * pJson = cJSON_Print(cRoot);
+    if(NULL == pJson)
+    {
+        cJSON_Delete(cRoot);
+        return NULL;
+    }
+
+    return pJson;
 }
--- client_info.h
+++ client_info.h
@@ -1,6 +1,55 @@
 #ifndef IMPROTOCOL_LIBRARY_H
 #define IMPROTOCOL_LIBRARY_H
 
-void get_client_info(char *json);
+typedef struct {
+    char *IP;
+    int ACTION;
+    char *account;
+    char *passwd;
+    char *t;
+}LOGIN_INFO;
+
+typedef struct {
+    char *IP;
+    int ACTION;
+    char *account;
+    char *t;
+}LOGOUT_INFO;
+
+typedef struct {
+    char *IP;
+    int ACTION;
+    char *account;
+    char *t;
+}CHAT_INFO;
+
+typedef struct {
+    char *account;
+    int ACTION;
+}ADD_INFO;
+
+
+typedef struct {
+    char IP[32];
+    char u_name[32];
+}FRIEND;
+
+typedef struct{
+    int STATUS;
+    char *t;
+}RE_INFO;
+
+//登陆信息
+void send_client_login_info(LOGIN_INFO *login_info,char *json);
+//登出信息
+void send_client_logout_info(LOGOUT_INFO *logout_info,char *json);
+//聊天连接信息
+void send_client_chat_info(CHAT_INFO *chat_info,char *json);
+//添加删除好友/群
+void send_add_chat_info(ADD_INFO *chat_info,char *json);
+//反回状态，是否成功
+char* re_status_info(RE_INFO *re_info);
+//反回朋友信息
+char* re_friend_info(FRIEND *aFriend , int len);
 
 #endif
